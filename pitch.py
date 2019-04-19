ed312cbd9e65d94eb13ac6e42155e83a6a2102ec
Daniel Montet <dmontetproff@gmail.com>
2019-04-19 23:23:22 +0300

    Add register and login templates, with wtf validation

--- pitch.py
+++ pitch.py
@@ -1,6 +1,8 @@
-from flask import Flask,render_template
-
+from flask import Flask,render_template, flash, redirect,url_for
+from forms import Register,Login
 app= Flask(__name__)
+ 
+app.config['SECRET_KEY']='fe80d26ee9e9e49b181c65aee655938c'
 
 @app.route("/")
 @app.route("/home")
@@ -11,6 +13,24 @@ def home():
 def circles():
     return render_template('circle.html')
 
+@app.route("/register" ,methods=['GET','POST'])
+def register():
+    form = Register()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data} !','success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/login",methods=['GET','POST'])
+def login():
+    form = Login()
+    if form.validate_on_submit():
+        flash(f'Login successful for {form.username.data} !','success')
+        return redirect(url_for('home'))
+
+    return render_template('login.html', title='login', form=form)
+
+ 
 
 if __name__ == "__main__":
     app.run(debug=True)
