749c244bc8b13aa78915fc6fa481e3ba52b08904
Daniel Montet <dmontetproff@gmail.com>
2019-04-20 22:18:41 +0300

    Add login and logout implimentation for authenticated users

--- app/views.py
+++ app/views.py
@@ -2,7 +2,7 @@ from flask import render_template, flash, redirect,url_for
 from app.forms import Register,Login
 from app.models import User, Pitch
 from app import app,db,bcrypt
-
+from flask_login import login_user, current_user, logout_user
 
 
 @app.route("/")
@@ -16,6 +16,8 @@ def circles():
 
 @app.route("/register" ,methods=['GET','POST'])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = Register()
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
@@ -29,8 +31,20 @@ def register():
 @app.route("/login",methods=['GET','POST'])
 def login():
     form = Login()
-    if form.validate_on_submit():
-        flash(f'Login successful !','success')
+    if current_user.is_authenticated:
         return redirect(url_for('home'))
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password,form.password.data):
+            login_user(user, remember=form.remember.data)
+            return redirect(url_for('home'))
+        
+        else:
+            flash('Login Unsuccessful. Please check email and password','danger')
 
     return render_template('login.html', title='login', form=form)
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
