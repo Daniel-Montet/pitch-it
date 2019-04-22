7a5037d834cc254ed642871d5d08d4cc3ddfc662
Daniel Montet <dmontetproff@gmail.com>
2019-04-22 15:48:46 +0300

    Add user account profile page

--- app/forms.py
+++ app/forms.py
@@ -1,26 +1,24 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo,ValidationError,Required
+from wtforms import StringField, PasswordField, SubmitField, BooleanField,ValidationError
+from wtforms.validators import DataRequired, Length, Email, EqualTo
 from app.models import User
 
 class Register(FlaskForm):
-    username = StringField('Username',validators=[Required(),
-                        Length(min=2, max=20)])
-    email = StringField('Email',validators=[Required(),Email()])
+    username = StringField('Username',validators=[DataRequired(),
+                        Length(min=2, max=20)],)
+    email = StringField('Email',validators=[DataRequired(),Email()])
 
-    password = PasswordField('Password', validators=[Required()])
-    confirm_password = PasswordField('Confirm Password', validators=[Required(), EqualTo('password')])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
 
-    def validate_username(self, username):
-        user = User.query.filter_by(username = username.data).first()
-        if user:
-            raise ValidationError('Username already taken')
+    def validate_email(self,email):
+            if User.query.filter_by(email = email.data).first():
+                raise ValidationError('There is an account with that email')
 
-    def validate_username(self, email):
-        email = User.query.filter_by(username = email.data).first()
-        if email:
-            raise ValidationError('Email already taken')
+    def validate_username(self,username):
+        if User.query.filter_by(username = username.data).first():
+            raise ValidationError('That username is taken')
 
 class Login(FlaskForm):
     email = StringField('Email',validators=[DataRequired(),Email()])
@@ -28,3 +26,21 @@ class Login(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
+
+
+class UpdateAccount(FlaskForm):
+    username = StringField('Username',validators=[DataRequired(),
+                        Length(min=2, max=20)])
+    email = StringField('Email',validators=[DataRequired(),Email()])
+
+    submit = SubmitField('Update')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username = username.data).first()
+        if user:
+            raise ValidationError('Username already taken')
+
+    def validate_email(self, email):
+        email = User.query.filter_by(email = email.data).first()
+        if email:
+            raise ValidationError('Email already taken')
--- app/views.py
+++ app/views.py
@@ -5,28 +5,53 @@ from app import app,db,bcrypt
 from flask_login import login_user, current_user, logout_user,login_required
 
 
-@app.route("/")
-@app.route("/home")
+@app.route("/",methods=['GET','POST'])
+@app.route("/home",methods=['GET','POST'])
 def home():
-    return render_template('home.html')
+    if current_user.is_authenticated:
+        return redirect(url_for('circles'))
+    registerForm = Register()
+    if registerForm.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(registerForm.password.data).decode('utf-8')
+        user = User(username = registerForm.username.data, email = registerForm.email.data, password = hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash(f'Your account has been created! You are now able to login!','success')
+        return redirect(url_for('home'))
+    # else:
+    #     flash('Registration Unsuccessful. Details you entered are already taken','danger')
+    
+    form = Login()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password,form.password.data):
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('circles'))
+        
+        else:
+            flash('Login Unsuccessful. Please check email and password','danger')
+    #form=form, registerForm=registerForm
 
-@app.route("/circle")
+    return render_template('home.html', title='login',form=form, registerForm=registerForm )
+
+@app.route("/circle",methods=['GET','POST'])
 def circles():
     return render_template('circle.html')
 
 @app.route("/register" ,methods=['GET','POST'])
 def register():
     if current_user.is_authenticated:
-        return redirect(url_for('home'))
-    form = Register()
-    if form.validate_on_submit():
+        return redirect(url_for('circles'))
+    registerForm = Register()
+    if registerForm.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
         user = User(username = form.username.data, email = form.email.data, password = hashed_password)
         db.session.add(user)
         db.session.commit()
         flash(f'Your account has been created! You are now able to login!','success')
-        return redirect(url_for('login'))
-    return render_template('register.html', title='Register', form=form)
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', registerForm=registerForm)
 
 @app.route("/login",methods=['GET','POST'])
 def login():
@@ -38,7 +63,7 @@ def login():
         if user and bcrypt.check_password_hash(user.password,form.password.data):
             login_user(user, remember=form.remember.data)
             next_page = request.args.get('next')
-            return redirect(next_page) if next_page else redirect(url_for('home'))
+            return redirect(next_page) if next_page else redirect(url_for('circles'))
         
         else:
             flash('Login Unsuccessful. Please check email and password','danger')
@@ -53,5 +78,5 @@ def logout():
 @app.route("/account")
 @login_required
 def account():
-
-    return render_template('account.html', title='Account')
+    image_file = url_for('static',filename='images/'+current_user.image_file)
+    return render_template('account.html', title='Account',image_file=image_file)
