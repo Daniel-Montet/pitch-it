13e8dddc32b40ec3dc058cfb4b41011b51be23ea
Daniel Montet <dmontetproff@gmail.com>
2019-04-23 22:30:42 +0300

    Add password reset functions

--- app/views.py
+++ app/views.py
@@ -1,7 +1,8 @@
 import secrets
 import os
 from flask import render_template, flash, redirect,url_for, request, abort
-from app.forms import Register,Login,UpdateAccount, PostForm, CommentForm
+from app.forms import( Register,Login,UpdateAccount,
+         PostForm, CommentForm, RequestResetForm, ResetPasswordForm)
 from app.models import User, Pitch, Comment
 from app import app,db,bcrypt
 from flask_login import login_user, current_user, logout_user,login_required
@@ -146,3 +147,29 @@ def delete_post(post_id):
     flash('Your post has been deleted!','success')
     return redirect(url_for('home'))
 
+
+def send_email(user):
+    pass
+
+@app.route("/reset_password",methods=['GET','POST'])
+def reset_request():
+    if current_user.is_autheniticated:
+        return redirect(url_for('home'))
+    form = RequestResetForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email = form.email.data).first()
+        send_email(user)
+        flash('An email has been sent with instructions to reset your password.','info')
+        return redirect(url_for('home'))
+    return render_template('reset_request.html', title='Reset Password', form= form)
+
+@app.route("/reset_password/<token>",methods=['GET','POST'])
+def reset_token(token):
+    if current_user.is_autheniticated:
+        return redirect(url_for('home'))
+    user = User.verify_reset_token(token)
+    if not user:
+        flash('That is an invalid token','warning')
+        return redirect(url_for('reset_request'))
+    form = ResetPasswordForm()
+    return render_template('reset_token.html', title='Reset Password', form= form)
