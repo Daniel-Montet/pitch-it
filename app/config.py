428bbd5cca12f9190af36abaaf28645216d013ab
Daniel Montet <dmontetproff@gmail.com>
2019-04-25 12:22:36 +0300

    Updating requirements.txt

--- app/config.py
+++ app/config.py
@@ -6,11 +6,11 @@ class Config:
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = "dmontetproff@gmail.com"
+    MAIL_USERNAME = "dannymkubwa@gmail.com"
     MAIL_PASSWORD = "dominuslew"
 
 class ProdConfig(Config):
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
 
 class DevConfig(Config):
